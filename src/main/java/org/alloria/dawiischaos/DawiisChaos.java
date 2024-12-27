00eef067beac270b6e3e084b2919aa43e8760684
AroDiYT <dariovanharneveldt@gmail.com>
2024-12-27 13:58:30 +0100

    Init

---
+++ src/main/java/org/alloria/dawiischaos/DawiisChaos.java
@@ -0,0 +1,16 @@
+package org.alloria.dawiischaos;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DawiisChaos implements ModInitializer {
+	public static final String MOD_ID = "dawiischaos";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/org/alloria/dawiischaos/DawiisChaosClient.java
@@ -0,0 +1,10 @@
+package org.alloria.dawiischaos;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DawiisChaosClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
